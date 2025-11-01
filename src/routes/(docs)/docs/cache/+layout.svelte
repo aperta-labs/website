966708e3796af8e2d7373a163dd359a2fea625d9
Liam Grossman <liam.j.grossman@gmail.com>
2025-11-01 19:37:40 -0400

    Add more docs

---
+++ src/routes/(docs)/docs/cache/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import DocLayout from '$lib/components/common/DocLayout.svelte';
+	import TableOfContents from '$lib/components/common/TableOfContents.svelte';
+	import type { LayoutProps } from '../logging copy/$types';
+
+	const tableOfContents = [
+		{
+			title: 'Introduction'
+		},
+		{
+			title: 'Getting Started',
+			slug: 'getting-started'
+		},
+		{
+			title: 'Examples',
+			slug: 'examples'
+		},
+		{
+			title: 'Changelog',
+			slug: 'changelog'
+		}
+	];
+
+	const { children }: LayoutProps = $props();
+</script>
+
+<div class="flex w-full">
+	<TableOfContents {tableOfContents} project="pager" />
+	<DocLayout>
+		{@render children()}
+	</DocLayout>
+</div>
